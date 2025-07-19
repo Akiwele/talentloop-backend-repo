a1ee1ef4fd1b9f7e8dbf3426ec39187449745a20
gideonadjei <gideonadjei94@gmail.com>
2025-07-19 11:26:30 +0000

    Initial Commit

---
+++ src/main/java/com/stacy/talentloop/TalentloopApplication.java
@@ -0,0 +1,13 @@
+package com.stacy.talentloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentloopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacy/talentloop/TalentloopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacy.talentloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
